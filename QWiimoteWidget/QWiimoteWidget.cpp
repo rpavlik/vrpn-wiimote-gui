12808bc06ad20ffa78116ce5b66fbd1b9f5b41dc
Ryan Pavlik <rpavlik@iastate.edu>
2011-02-24 14:57:17 -0600

    clean up

--- QWiimoteWidget/QWiimoteWidget.cpp
+++ QWiimoteWidget/QWiimoteWidget.cpp
@@ -24,44 +24,23 @@ QWiimoteWidget::QWiimoteWidget(QWidget *parent) :
 	updateGeometry();
 
 	setScene(new QGraphicsScene(this));
-	setTransformationAnchor(AnchorUnderMouse);
-	setDragMode(ScrollHandDrag);
-	setViewportUpdateMode(FullViewportUpdate);
-	// Prepare background check-board pattern
-	QPixmap tilePixmap(64, 64);
-	tilePixmap.fill(Qt::white);
-	QPainter tilePainter(&tilePixmap);
-	QColor color(220, 220, 220);
-	tilePainter.fillRect(0, 0, 32, 32, color);
-	tilePainter.fillRect(32, 32, 32, 32, color);
-	tilePainter.end();
-
-	setBackgroundBrush(tilePixmap);
 
 	QGraphicsScene *s = scene();
 
 	_wiimote = new QGraphicsSvgItem(WIISVG);
-	//_wiimote->setFlags(QGraphicsItem::ItemClipsToShape);
-	//_wiimote->setZValue(0);
 	s->addItem(_wiimote);
 
-	//std::cerr <<_wiimote->boundingRect().width() << " by " << _wiimote->boundingRect().height() << std::endl;
-	//s->setSceneRect(_wiimote->boundingRect().adjusted(-10, -10, 10, 10));
-/*
 	setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	fitInView(_wiimote, Qt::KeepAspectRatio);
-*/
+
 	_activated = new QSvgRenderer(ACTIVESVG, this);
-	//_activated = _wiimote->renderer();
 
 	for (unsigned int i = 0; i < 4; ++i) {
 		_leds[i] = _createAndAddSubitem(QString("led") + QString::number(i+1));
-		_leds[i]->setVisible(true);
 	}
 
 	_buttonA = _createAndAddSubitem("aButtonDown");
-	_buttonA->setVisible(true);
 }
 
 QWiimoteWidget::~QWiimoteWidget()
@@ -120,29 +99,21 @@ void QWiimoteWidget::setMinusButton(bool state) {
 
 void QWiimoteWidget::resizeEvent(QResizeEvent * event) {
 	QGraphicsView::resizeEvent(event);
-	//fitInView(_wiimote, Qt::KeepAspectRatio);
+	fitInView(_wiimote, Qt::KeepAspectRatio);
 }
 
 QGraphicsItem * QWiimoteWidget::_createAndAddSubitem(QString const& id) {
-	//QGraphicsSvgItem * ret = new QGraphicsSvgItem(ACTIVESVG);
-	QGraphicsSvgItem * sub = new QGraphicsSvgItem;
-	sub->setSharedRenderer(_activated);
-	sub->setElementId(id);
-	sub->setFlags(QGraphicsItem::ItemClipsToShape);
-	sub->setCacheMode(QGraphicsItem::NoCache);
-	QMatrix mat = _activated->matrixForElement(id);
-	QRectF bbox =  (_activated->boundsOnElement(id) * mat).boundingRect();
+	QGraphicsSvgItem * ret = new QGraphicsSvgItem;
+	ret->setSharedRenderer(_activated);
+	ret->setElementId(id);
+	QPolygonF elt = _activated->boundsOnElement(id) *
+					_activated->matrixForElement(id);
 
-	QString info = QString::number(bbox.topLeft().x()) + QString(", ") + QString::number(bbox.topLeft().x());
-	QMessageBox::information(this, id, info);
-	//double scale = 1.0; //-0.1;
-	QGraphicsItemGroup * ret = new QGraphicsItemGroup;
-	ret->addToGroup(sub);
-	//ret->setTransform(QTransform(mat));
-	//ret->setPos(mat.dx() * scale, mat.dy() * scale);
-	ret->setPos(bbox.topLeft());
+	ret->setPos(elt.boundingRect().topLeft());
 	ret->setZValue(1);
-	ret->setVisible(false);
+
+	//ret->setVisible(false);
+	ret->setVisible(true);
 
 	scene()->addItem(ret);
 	return ret;
