71bf87978b92c92b9902257db9521ebd66dede6c
Ryan Pavlik <rpavlik@iastate.edu>
2011-02-25 12:08:00 -0600

    comments for mainloop container

--- vrpn_MainloopContainer.h
+++ vrpn_MainloopContainer.h
@@ -5,20 +5,30 @@
 
 #include <vector>
 
+/// A container that holds and owns one or more VRPN objects,
 class vrpn_MainloopContainer {
 	public:
+		/// Constructor
 		vrpn_MainloopContainer() {}
+		/// Destructor: invokes clear()
 		~vrpn_MainloopContainer();
 
+		/// Clear internal structure holding objects, deleting them
+		/// in reverse order of their addition.
 		void clear();
 
+		/// Add an object wrapped by vrpn_MainloopObject.
 		void add(vrpn_MainloopObject * o);
 
+		/// Template method to automatically wrap objects
+		/// with vrpn_MainloopObject before adding them.
 		template<class T>
 		void add(T o) {
 			add(vrpn_MainloopObject::wrap(o));
 		}
 
+		/// Runs mainloop on all contained objects, in the order
+		/// that they were added.
 		void mainloop();
 
 	private:
